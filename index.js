29fc97119a28d4e3aadaa4fd186125e1feb41ea7
Alex Manzi <alex.manzi22@gmail.com>
2022-02-01 19:36:22 -0500

    commit tree d39da2eb0eab7496b522f9ea681f9993f28052c8

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const employee = {
     name: "Alex",
-    streetAddress: "228 Ave B",
+    streetAddress: "11 Broadway",
 };
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
@@ -10,16 +10,16 @@ function updateEmployeeWithKeyAndValue(employee, key, value) {
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
+    employee[key] = value
+    return employee
 }
-    
- function deleteFromEmployeeByKey(employee, key) {
-      const newEmployee = {...employee};
-      delete newEmployee[key];
-      return newEmployee;
- }
- function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
- }
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key]
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
